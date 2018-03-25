7dabb2ebab324df76b09b5be8206511f26573024
xiexiaolong <442917934@qq.com>
2018-03-25 13:40:57 +0800

    Hello world 示例

---
+++ myfirst.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* 这是我的第一个简单的程序 */
+	fmt.Println("Hello, World!")
+	fmt.Println("菜鸟教程：runoob.com")
+
+	/*GO语言变量*/
+	var age = 35
+	var name = "Boole"
+	fmt.Println("%d",age,name)
+	sex := 1
+	fmt.Println(sex)
+
+	/*GO语言常量*/
+	const WIDTH  int = 210
+	const LENGTH int = 230
+	var area int
+	area = WIDTH * LENGTH;
+	fmt.Println("面积为:",area)
+    //特殊常量
+
+	/*GO语言运算符*/
+
+
+}
